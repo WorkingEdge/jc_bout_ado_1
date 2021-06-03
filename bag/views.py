2d44a1aa1573f4a1560df1c5ae55941a94b070b5
WorkingEdge <jclry76@gmail.com>
2021-06-03 08:53:56 +0000

    Add remove functionality. Finished video Adjusting and removing part 2

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 def view_bag(request):
@@ -45,7 +45,7 @@ def add_to_bag(request, item_id):
     
     # Return user to the same page but with the updated info
     return redirect(redirect_url) 
-
+# ----------------------------------------------- Adjust Bag
 def adjust_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -58,12 +58,44 @@ def adjust_bag(request, item_id):
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
         else:
-            del bag[item_id]['items_by_size'][size] 
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+
     else:
         if quantity > 0:
             bag[item_id] = quantity
         else:
-            bag.pop[item_id]
+            bag.pop(item_id)
+    
     request.session['bag']=bag
     return redirect(reverse('view_bag'))
+
+# -------------------------------------------------- Remove from bag
+def remove_from_bag(request, item_id):
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+
+        bag = request.session.get('bag', {}) 
+        """
+        If the bag dict has a size value for this item in the items_by_size dict, delete it
+        bag - dict, item_id - key, items_by_size - dict, size - key
+        If this means the items_by_size dict is now empty, delete the item from the bag altogether
+
+        If there is no items_by_size dict for this item, remove the item_id (key) from the bag dict
+        """
+        if size: 
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id) 
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+
+        return HttpResponse(status=200) # Return 200 to the js function
+    except Exception as e:
+        return HttpResponse(status=500) # If the try doesn't work, return 500 to the js function  
        
