d0b161e4d9593a8c289cbdd91b8da02c93aff438
WorkingEdge <jclry76@gmail.com>
2021-06-10 12:08:34 +0000

    Add signals and forms for checkout - to end of vid admin, signals, forms part2

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    """
+    Specify the fields that are read-only in the admin - these values are calculated and should not be editable.
+    """
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+    """
+    Specifying the fields maintains the order we want. otherwise, they would appear but not in expected order due to how django handles the read-only fields
+    """
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    """
+    Restrict the cols that appear
+    """
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
