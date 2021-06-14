1d29c4109a2844e758242b8b201f433863b8d32e
WorkingEdge <jclry76@gmail.com>
2021-06-14 12:46:11 +0000

    Make successful payment functinality - to end stripe vid 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -173,6 +173,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
+"""stripe"""
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '') # Store these in env variables in gitpod account or import each time in shell
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,50 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
 from .forms import OrderForm
+from django.conf import settings
+from bag.contexts import bag_contents #Makes the bag_contents() functions available here
+
+import stripe #First need to install it using 'pip3 install stripe'
 
 
 def checkout(request):
+    """
+    Set values for stripe public and secret keys. These are from the settings file (imported above) and settings gets them from the environment, set on gitpod settings
+    The secret key is used further down for the stripe api key
+    """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    """
+    get the current bag from the bag_contents() function. This functions returns a dictionary and the dict has a key of 'grand_total'. This can be used to give the total. For stripe, this needs to be rounded and converted to an integer (*100): 
+    """
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total*100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY, 
+    )
+
+    
+
     order_form = OrderForm()
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Dis you forget to set it in your environment?')
+    
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51J0tVwGXFBy9KvEyyA4IB4EpQDGsAaN1IP3rXj4zJRyiTWECYhcjoN4uPmului3nRZ8MdgutiSs2aU2Wy1Ob9iyZ00QlVoIzIB',
-        'client_secret': 'test value',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
