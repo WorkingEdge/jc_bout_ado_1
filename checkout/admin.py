b1baad1ec598a4abfa509b155630405223dc3c0a
WorkingEdge <jclry76@gmail.com>
2021-06-15 17:11:28 +0000

    Stripe 16

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,7 +14,7 @@ class OrderAdmin(admin.ModelAdmin):
     """
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag', 'stripe_pid',)
     """
     Specifying the fields maintains the order we want. otherwise, they would appear but not in expected order due to how django handles the read-only fields
     """
@@ -22,7 +22,7 @@ class OrderAdmin(admin.ModelAdmin):
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag', 'stripe_pid',)
 
     """
     Restrict the cols that appear
