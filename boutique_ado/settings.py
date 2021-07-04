c942ffe301ef78680b7c571b8fe58d36062b14f3
WorkingEdge <jclry76@gmail.com>
2021-07-04 07:39:45 +0000

    Update settings.py re base_dir to match tutorial to try fix h10 error'

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
